bec62b195d7e442749ea1f99b37d5661825510a0
Feng Wang <wang6922@126.com>
2024-01-16 23:18:13 +0800

    add style

--- src/vt100.ts
+++ src/vt100.ts
@@ -1,5 +1,11 @@
 import { isAsciiEscapeCode } from "./asciiEscapeCode";
 
+type vt100Char = {
+    content:string;
+    negative?:boolean;
+    underline?:boolean;
+};
+
 const charSets:{[charSet:string]:{[char:string]:string}} = {
     '(A':{
         '#':'£'
@@ -85,22 +91,23 @@ class VT100 {
     private putc: (char:number) => void;
     private lines:number;
     private columns:number;
-    private buffer:Array<Array<string>>;
+    private buffer:Array<Array<vt100Char>>;
     private cursor:{x:number, y:number} = {x:0, y:0};
     private savedCursor:{x:number, y:number} = {x:0, y:0};
     private escapeSequence:Array<number> = [];
     private charSet:string = '(B';
     private scrollRegion:{top:number, bottom:number} = {top:0, bottom:0};
+    private style:{negative:boolean, underline:boolean} = {negative:false, underline:false};
     constructor(putc:(char:number) => void, size?:{lines:number, columns:number}) {
         this.putc = putc;
         this.lines = size?.lines ?? 24;
         this.columns = size?.columns ?? 80;
         this.scrollRegion.top = 0;
         this.scrollRegion.bottom = this.lines - 1;
-        this.buffer = new Array<Array<string>>(this.lines);
+        this.buffer = new Array<Array<vt100Char>>(this.lines);
         for (let i = 0; i < this.lines; i++) {
-            this.buffer[i] = new Array<string>(this.columns);
-            this.buffer[i].fill('&nbsp;');
+            this.buffer[i] = new Array<vt100Char>(this.columns);
+            this.buffer[i].fill({content:'&nbsp;'});
         }
     }
     getc(char:number):void {
@@ -109,12 +116,16 @@ class VT100 {
         }
     }
     private handleScreenScroll():void{
-        if(this.cursor.y > this.scrollRegion.bottom){
+        while(this.cursor.y > this.scrollRegion.bottom){
             this.buffer.splice(this.scrollRegion.top, 1);
-            this.buffer.splice(this.scrollRegion.bottom, 0, new Array<string>(this.columns).fill('&nbsp;'));
+            this.buffer.splice(this.scrollRegion.bottom, 0, new Array<vt100Char>(this.columns).fill({content:'&nbsp;'}));
             this.cursor.y--;
         }
-        /** No extra memory on top, thus no scroll up */
+        while(this.cursor.y < this.scrollRegion.top){
+            this.buffer.splice(this.scrollRegion.bottom, 1);
+            this.buffer.splice(this.scrollRegion.top, 0, new Array<vt100Char>(this.columns).fill({content:'&nbsp;'}));
+            this.cursor.y++;
+        }
     }
     private handleEscapeCode(char:number):boolean{
         if(isAsciiEscapeCode(char) || char >= 0x80){
@@ -195,11 +206,18 @@ class VT100 {
                     break;
                 case 'D'.charCodeAt(0):
                     /**
-                     * Move the text down/ window up one line.
+                     * Move the display down/ text up one line.
                      */
-                    this.buffer.shift();
-                    this.buffer.push(new Array<string>(this.columns));
-                    this.buffer[this.lines-1].fill('&nbsp;');
+                    this.buffer.splice(this.scrollRegion.top, 1);
+                    this.buffer.splice(this.scrollRegion.bottom, 0, new Array<vt100Char>(this.columns).fill({content:'&nbsp;'}));
+                    this.escapeSequence = [];
+                    break;
+                case 'M'.charCodeAt(0):
+                    /**
+                     * Move the display up/ text down one line.
+                     */
+                    this.buffer.splice(this.scrollRegion.bottom, 1);
+                    this.buffer.splice(this.scrollRegion.top, 0, new Array<vt100Char>(this.columns).fill({content:'&nbsp;'}));
                     this.escapeSequence = [];
                     break;
                 case 'E'.charCodeAt(0):
@@ -232,7 +250,7 @@ class VT100 {
                      * Set the terminal to the initial state.
                      */
                     for(const line of this.buffer){
-                        line.fill('&nbsp;');
+                        line.fill({content:'&nbsp;'});
                     }
                     this.cursor.x = 0;
                     this.cursor.y = 0;
@@ -240,6 +258,8 @@ class VT100 {
                     this.scrollRegion.top = 0;
                     this.scrollRegion.bottom = this.lines - 1;
                     this.escapeSequence = [];
+                    this.style.negative = false;
+                    this.style.underline = false;
                     break;
                 default:
                     /** ignored single byte escape codes */
@@ -294,7 +314,11 @@ class VT100 {
                     if(i-n >= this.cursor.x){
                         this.buffer[this.cursor.y][i] = this.buffer[this.cursor.y][i-n];
                     }else{
-                        this.buffer[this.cursor.y][i] = '&nbsp;';
+                        this.buffer[this.cursor.y][i] = {
+                            content:'&nbsp;', 
+                            negative:this.style.negative, 
+                            underline:this.style.underline
+                        };
                     }
                 }
             } break;
@@ -303,17 +327,13 @@ class VT100 {
                 const params = this.getCSIParams();
                 const n = params[0] ?? 1;
                 this.cursor.y -= n;
-                if(this.cursor.y < 0){
-                    this.cursor.y = 0;
-                }
+                this.handleScreenScroll();
             } break;
             case 'B'.charCodeAt(0):{
                 /** Move cursor down. */
                 const n = this.getCSIParams()[0] ?? 1;
                 this.cursor.y += n;
-                while(this.cursor.y > this.scrollRegion.bottom){
-                    this.handleScreenScroll();
-                }
+                this.handleScreenScroll();
             } break;
             case 'C'.charCodeAt(0):
             case 'a'.charCodeAt(0):{
@@ -337,18 +357,14 @@ class VT100 {
                 const n = this.getCSIParams()[0] ?? 1;
                 this.cursor.y += n;
                 this.cursor.x = 0;
-                while(this.cursor.y > this.scrollRegion.bottom){
-                    this.handleScreenScroll();
-                }
+                this.handleScreenScroll();
             } break;
             case 'F'.charCodeAt(0):{
                 /** Move cursor up to begining of the line */
                 const n = this.getCSIParams()[0] ?? 1;
                 this.cursor.y -= n;
                 this.cursor.x = 0;
-                if(this.cursor.y < 0){
-                    this.cursor.y = 0;
-                }
+                this.handleScreenScroll();
             } break;
             case 'G'.charCodeAt(0):
             case '`'.charCodeAt(0):{
@@ -398,25 +414,25 @@ class VT100 {
                     case 0:
                         /** Erase from the cursor to the end of the page */
                         for(let i = this.cursor.x; i < this.columns; i++){
-                            this.buffer[this.cursor.y][i] = '&nbsp;';
+                            this.buffer[this.cursor.y][i] = {content:'&nbsp;'};
                         }
                         for(let i = this.cursor.y + 1; i < this.lines; i++){
-                            this.buffer[i].fill('&nbsp;');
+                            this.buffer[i].fill({content:'&nbsp;'});
                         }
                         break;
                     case 1:
                         /** Erase from the cursor to the begining of the page */
                         for(let i = this.cursor.x - 1; i >= 0; i--){
-                            this.buffer[this.cursor.y][i] = '&nbsp;';
+                            this.buffer[this.cursor.y][i] = {content:'&nbsp;'};
                         }
                         for(let i = this.cursor.y - 1; i >= 0; i--){
-                            this.buffer[i].fill('&nbsp;');
+                            this.buffer[i].fill({content:'&nbsp;'});
                         }
                         break;
                     case 2:
                         /** Erase the entire display */
                         for(const line of this.buffer){
-                            line.fill('&nbsp;');
+                            line.fill({content:'&nbsp;'});
                         }
                         break;
                     default:
@@ -430,18 +446,18 @@ class VT100 {
                     case 0:
                         /** Erase from the cursor to the end of the line */
                         for(let i = this.cursor.x; i < this.columns; i++){
-                            this.buffer[this.cursor.y][i] = '&nbsp;';
+                            this.buffer[this.cursor.y][i] = {content:'&nbsp;'};
                         }
                         break;
                     case 1:
                         /** Erase from the cursor to the begining of the line */
                         for(let i = this.cursor.x - 1; i >= 0; i--){
-                            this.buffer[this.cursor.y][i] = '&nbsp;';
+                            this.buffer[this.cursor.y][i] = {content:'&nbsp;'};
                         }
                         break;
                     case 2:
                         /** Erase the entire line */
-                        this.buffer[this.cursor.y].fill('&nbsp;');
+                        this.buffer[this.cursor.y].fill({content:'&nbsp;'});
                         break;
                     default:
                         break;
@@ -452,8 +468,7 @@ class VT100 {
                 let n = this.getCSIParams()[0] ?? 1;
                 n = Math.min(n, this.lines - this.cursor.y);
                 for(let i = 0; i < n; i++){
-                    this.buffer.splice(this.cursor.y, 0, new Array<string>(this.columns));
-                    this.buffer[this.cursor.y].fill('&nbsp;');
+                    this.buffer.splice(this.cursor.y, 0, new Array<vt100Char>(this.columns).fill({content:'&nbsp;'}));
                 }
                 this.buffer.splice(this.lines, n);
             } break;
@@ -463,7 +478,7 @@ class VT100 {
                 n = Math.min(n, this.lines - this.cursor.y);
                 this.buffer.splice(this.cursor.y, n);
                 for(let i = 0; i < n; i++){
-                    this.buffer.push(new Array<string>(this.columns).fill('&nbsp;'));
+                    this.buffer.push(new Array<vt100Char>(this.columns).fill({content:'&nbsp;'}));
                 }
             } break;
             case 'N'.charCodeAt(0):{
@@ -479,7 +494,7 @@ class VT100 {
                     if(i+n < this.columns){
                         this.buffer[this.cursor.y][i] = this.buffer[this.cursor.y][i+n];
                     }else{
-                        this.buffer[this.cursor.y][i] = '&nbsp;';
+                        this.buffer[this.cursor.y][i] = {content:'&nbsp;'};
                     }
                 }
             } break;
@@ -487,26 +502,23 @@ class VT100 {
                 /** Move the displayed text up/ windows down */
                 const n = this.getCSIParams()[0] ?? 1;
                 for(let i = 0; i < n; i++){
-                    this.buffer.shift();
-                    this.buffer.push(new Array<string>(this.columns));
-                    this.buffer[this.lines-1].fill('&nbsp;');
+                    this.buffer.splice(this.scrollRegion.top, 1);
+                    this.buffer.splice(this.scrollRegion.bottom, 0, new Array<vt100Char>(this.columns).fill({content:'&nbsp;'}));
                 }
             } break;
             case 'T'.charCodeAt(0):{
                 /** Move the displayed text down/ windows up */
-                /** ignored since we do not have extra memory (nothing on top) now. */
-                // const n = this.getCSIParams()[0] ?? 1;
-                // for(let i = 0; i < n; i++){
-                //     this.buffer.unshift(new Array<string>(this.columns));
-                //     this.buffer.pop();
-                //     this.buffer[0].fill('&nbsp;');
-                // }
+                const n = this.getCSIParams()[0] ?? 1;
+                for(let i = 0; i < n; i++){
+                    this.buffer.splice(this.scrollRegion.bottom, 1);
+                    this.buffer.splice(this.scrollRegion.top, 0, new Array<vt100Char>(this.columns).fill({content:'&nbsp;'}));
+                }
             } break;
             case 'X'.charCodeAt(0):{
                 /** Erase characters */
                 const n = this.getCSIParams()[0] ?? 1;
                 for(let i = this.cursor.x; i < this.cursor.x + n && i < this.columns; i++){
-                    this.buffer[this.cursor.y][i] = '&nbsp;';
+                    this.buffer[this.cursor.y][i] = {content:'&nbsp;'};
                 }
             } break;
             case 'Z'.charCodeAt(0):{
@@ -536,9 +548,34 @@ class VT100 {
                     this.cursor.y = this.lines - 1;
                 }
             } break;
-            case 'm'.charCodeAt(0):
-                /** @todo */
-                break;
+            case 'm'.charCodeAt(0):{
+                let params = this.getCSIParams();
+                if(params.length === 0){
+                    params = [0];
+                }
+                for(const param of params){
+                    switch (param){
+                        case 0:
+                            this.style.negative = false;
+                            this.style.underline = false;
+                            break;
+                        case 4:
+                            this.style.underline = true;
+                            break;
+                        case 24:
+                            this.style.underline = false;
+                            break;
+                        case 7:
+                            this.style.negative = true;
+                            break;
+                        case 27:
+                            this.style.negative = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            } break;
             case 'r'.charCodeAt(0):{
                 /** Set scroll region */
                 const params = this.getCSIParams();
@@ -563,7 +600,7 @@ class VT100 {
                 let n = this.getCSIParams()[0] ?? 1;
                 n = Math.min(n, this.lines - this.cursor.y);
                 for(let i = 0; i < n; i++){
-                    const line = this.buffer.pop() ?? new Array<string>(this.columns).fill('&nbsp;');
+                    const line = this.buffer.pop() ?? new Array<vt100Char>(this.columns).fill({content:'&nbsp;'});
                     this.buffer.splice(this.cursor.y, 0, line);
                 }
             } break;
@@ -584,7 +621,11 @@ class VT100 {
             '>': '&gt;',
             '&': '&amp;',
         }[charStr] ?? charStr;
-        this.buffer[this.cursor.y][this.cursor.x] = charStr;
+        this.buffer[this.cursor.y][this.cursor.x] = {
+            content:charStr, 
+            negative:this.style.negative, 
+            underline:this.style.underline
+        };
         this.cursor.x++;
         if (this.cursor.x >= this.columns) {
             this.cursor.x = 0;
@@ -608,7 +649,20 @@ class VT100 {
                 if(i === this.cursor.y && j === this.cursor.x){
                     buffer += '<span class="cursor">|</span>';
                 }
-                buffer += this.buffer[i][j];
+                const char = this.buffer[i][j];
+                let classes = [];
+                if(char.negative){
+                    classes.push('negative');
+                }
+                if(char.underline){
+                    classes.push('underline');
+                }
+                if(classes.length > 0){
+                    buffer += `<span class="${classes.join(' ')}">${char.content}</span>`;
+                }
+                else{
+                    buffer += char.content;
+                }
             }
             if(i !== this.buffer.length - 1){
                 buffer += '<br>';
